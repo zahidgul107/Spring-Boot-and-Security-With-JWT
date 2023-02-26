7b05a161c065c0fb98b37f7fba71615df41c1203
ZAHID GUL <zahidgul107@gmail.com>
2023-02-26 21:09:31 +0530

    initial commit

---
+++ Spring-Boot-and-Security-With-JWT/src/main/java/com/boot/security/jwt/SpringBootAndSecurityWithJwtApplication.java
@@ -0,0 +1,14 @@
+package com.boot.security.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAndSecurityWithJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAndSecurityWithJwtApplication.class, args);
+		System.err.println("Hello zahid");
+	}
+
+}
---
+++ Spring-Boot-and-Security-With-JWT/src/test/java/com/boot/security/jwt/SpringBootAndSecurityWithJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.security.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAndSecurityWithJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
